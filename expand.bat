ae7cf88f6656e59224feaf5c26d97d20a1a2942c
Dr. Martin Luther <jesu.broder@gmail.com>
2024-11-29 09:30:07 +0800

    docs(content): add all md files
    - all med files is added with full content
    - all files need to be editid

--- expand.bat
+++ expand.bat
@@ -1,1 +1,3 @@
 Set NODE_OPTIONS="--max-old-space-size=8192"
+:: export NODE_OPTIONS="--max-old-space-size=8192"
+:: Source: https://stackoverflow.com/questions/53230823/fatal-error-ineffective-mark-compacts-near-heap-limit-allocation-failed-javas
